b8e9b187fc0cc20e7f9e81c57c4ee349d819741d
Danielle-Luz <daniln2209@gmail.com>
2023-02-20 00:25:10 -0300

    feat: create error handler

--- src/errors.ts
+++ src/errors.ts
@@ -1,9 +1,23 @@
+import { Request, Response } from "express";
+
 class AppError {
   statusCode: number;
   message: string;
-  
-  constructor (statusCode: number, message: string) {
+
+  constructor(statusCode: number, message: string) {
     this.statusCode = statusCode;
     this.message = message;
   }
 }
+
+export const errorHandler = (error: Error, req: Request, res: Response) => {
+  let statusCode = 500;
+  const errorMessage = { message: "There was a internal server error" };
+
+  if (error instanceof AppError) {
+    errorMessage.message = error.message;
+    statusCode = error.statusCode;
+  }
+  
+  res.status(statusCode).send(errorMessage);
+};
