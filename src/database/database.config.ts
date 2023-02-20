0aee5f71ffc4ad0a2c166e67f666a91b6c178b57
Danielle-Luz <daniln2209@gmail.com>
2023-02-20 00:33:21 -0300

    feat: create connection object

---
+++ src/database/database.config.ts
@@ -0,0 +1,10 @@
+import { Client } from "pg";
+import "dotenv/config";
+
+export const connection = new Client({
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  port: parseInt(String(process.env.PORT))
+});
