2245b1312a562a860a14b4f6ebfb3b61959c04a0
Danielle-Luz <daniln2209@gmail.com>
2023-02-20 23:06:11 -0300

    feat: add validate body middleware in projects post route

--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
 import { projectController } from "../controllers/projects.controllers";
+import { middlewares } from "../middlewares/index.middlewares";
+import { projectSchema } from "../schemas/projects.schemas";
 
 export const projectsRouter: Router = Router();
 
-projectsRouter.post("", projectController.insertProject);
+projectsRouter.post(
+  "",
+  middlewares.validateBody(projectSchema.schema),
+  projectController.insertProject
+);
