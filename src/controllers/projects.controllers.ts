5a0265a8dbb5a225ab7748ef0c0217a8b1db4ae8
Danielle-Luz <daniln2209@gmail.com>
2023-02-20 21:14:34 -0300

    feat: create get projects by stack controller function

--- src/controllers/projects.controllers.ts
+++ src/controllers/projects.controllers.ts
@@ -1,3 +1,19 @@
+import { Request, Response } from "express";
+import { services } from "../services/index.services";
+
 export namespace projectController {
   const selectedProjectFields = ["id", "name", "url", "stack_name"];
+
+  export const getProjectsByStack = async (req: Request, res: Response) => {
+    const searchedStack = req.params.stack;
+
+    const foundProjects = await services.selectDataWithWhere(
+      "projects",
+      "stack_name",
+      searchedStack,
+      selectedProjectFields
+    );
+
+    return res.status(200).send(foundProjects);
+  };
 }
